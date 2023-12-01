13427e43ead6a8dd96b0b18f698107f3ff8ed160
AnandSwaraj <45799252+AnandSwaraj@users.noreply.github.com>
2023-12-01 23:20:37 +0530

    first commit. commiting general files                  and file structure.

---
+++ AstrotalkAssignment/src/main/java/com/anand/AstrotalkAssignment/App.java
@@ -0,0 +1,13 @@
+package com.anand.AstrotalkAssignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
